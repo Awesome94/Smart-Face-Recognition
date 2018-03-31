54ab7a5d48cced86031b0100f374417a323e2882
David Kironde <david.kironde@andela.com>
2018-04-01 01:17:21 +0300

    add rank component and configure particles library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import Navigation from "./components/navigation/navigation";
 import Logo from "./components/logo/logo";
-
+import ImageLinkForm from "./components/imagelinkform/imagelinkformComponent";
+import Rank from "./components/rank/rack";
 import './App.css';
 import "./index.css";
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800,
+        }
+      }
+    }
+  }
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Particles className="particles"
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
-       {/* <ImageLinkForm />
-        <FaceRecogonition />} */}
+        <Rank />
+        <ImageLinkForm />
+      {/* <FaceRecogonition />} */}
       </div>
     );
   }
---
+++ src/components/rank/rack.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rank = () => {
+  return (
+    <div>
+    <div className = 'white fa2  center'>
+      {"Awesome, you ranked number something"}
+    </div>
+    <div className = 'white f1 center'>
+      {"#5"}
+    </div>
+    </div>
+  )
+}
+export default Rank;
