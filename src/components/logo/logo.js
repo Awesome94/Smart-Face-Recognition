2732b0d9727e10c495483ca0853b005bab781682
David Kironde <david.kironde@andela.com>
2018-03-31 23:04:31 +0300

    add logo component

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Tilt from 'react-tilt'
+import exercise from "./exercise.png";
 import './logo.css'
 
 const Logo = () => {
   return (
     <div className='ma4 mto'>
-    <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-    <div className="Tilt-inner"> 👽 </div>
+    <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 200, width: 200 }} >
+    <div className="Tilt-inner">
+    <img style={{paddingTop: '70px', width:' 50%'}}alt='logo' src={exercise}/>
+    </div>
     </Tilt>
     </div>
   )
