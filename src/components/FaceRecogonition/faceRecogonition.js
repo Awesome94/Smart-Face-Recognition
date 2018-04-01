9ba977a03bd1a31253843cc16a5e3ceaf86e0880
David Kironde <david.kironde@andela.com>
2018-04-02 00:08:19 +0300

    update styling for the facerecogonition image

--- src/components/FaceRecogonition/faceRecogonition.js
+++ src/components/FaceRecogonition/faceRecogonition.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const FaceRecogonition = ({imageUrl}) => {
   return (
-    <div className = 'pa5 center'>
-    <img style={{width: '25%', height:'25%'}}alt='sample img' src = {imageUrl} />
+    <div className='center ma'>
+    <div className = 'center absolute mt2'>
+    <img alt='sample img' src = {imageUrl} width='500px' height='auto' />
+    </div>
     </div>
   )
 }
