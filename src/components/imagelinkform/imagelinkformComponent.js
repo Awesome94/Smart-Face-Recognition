d291ae398c442cfdec95482d78f357a88d31a9d3
David Kironde <david.kironde@andela.com>
2018-04-01 19:32:23 +0300

    add onIputchange and onSubmit props

--- src/components/imagelinkform/imagelinkformComponent.js
+++ src/components/imagelinkform/imagelinkformComponent.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './imageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div className='center'>
     <div className = 'form w-50 center pa4 br3 shadow-5'>
-      <input className='f4 pa2 w-70 center' type='text'/>
-      <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+      <input className='f4 pa2 w-70 center' type='text' onChange = {onInputChange}/>
+      <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick = {onButtonSubmit}>Detect</button>
       </div>
     </div>
   )
